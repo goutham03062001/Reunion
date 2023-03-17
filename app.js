8ceaf5cc6aaf9dbc4d1d1981e7de3af186a7bfeb
Goutham <gouthamkumarpolapally@gmail.com>
2023-03-17 19:40:48 +0530

    FirstCommit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({path:'./config.env'});
+
+const conn = require("./connection/connection.js");
+const auth = require("./routers/Auth");
+const post = require("./routers/Post");
+const comment = require("./routers/Comments");
+const allPosts = require("./routers/AllPosts");
+const likePost = require("./routers/Like");
+const unlikePost = require("./routers/UnlikePost");
+const User = require("./routers/User");
+const Follow = require("./routers/Follow");
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use("/api/authenticate",auth);
+app.use("/api/posts/",post);
+app.use("/api/comment",comment);
+app.use("/api/all_posts",allPosts);
+app.use("/api/like",likePost);
+app.use("/api/unlike",unlikePost);
+app.use("/api/user",User);
+app.use("/api/follow",Follow);
+
+const port = 4500;
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+app.listen(port, ()=>{console.log("Listening on port : ",port)});
